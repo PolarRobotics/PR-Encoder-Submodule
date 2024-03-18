3e95a7259306fb02aec7e2339e59928a20ed9908
k-colish <k-colish@onu.edu>
2024-03-18 19:12:29 -0400

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -5,14 +5,18 @@
 #define BYTES 2
 #define UART_TX_PIN 0
 #define UART_RX_PIN 1
-#define A_PIN 2
-#define B_PIN 3
+#define A_PIN1 2
+#define B_PIN1 3
+#define A_PIN2 4
+#define B_PIN2 5
+
 
 uint8_t data[BYTES] = {0,0};
 uint8_t recieved[BYTES];
 uint8_t encoder_data_a;
 uint8_t encoder_data_b;
-Encoder runningBack(A_PIN, B_PIN, "");
+Encoder runningBack1(A_PIN1, B_PIN1, "");
+Encoder runningBack2(A_PIN2, B_PIN2, ""); 
 int encoderSpeed;
 uint8_t test = 0;
 
@@ -24,21 +28,28 @@ void setup() {
   uart_init(uart0, 115200);
   gpio_set_function(UART_TX_PIN, GPIO_FUNC_UART);
   gpio_set_function(UART_RX_PIN, GPIO_FUNC_UART);
-  runningBack.initEncoder();
+  runningBack1.initEncoder();
+  //runningBack2.initEncoder();
 
-  gpio_set_irq_enabled_with_callback(A_PIN, GPIO_IRQ_EDGE_RISE, true, readEncoder);
+  gpio_set_irq_enabled_with_callback(A_PIN1, GPIO_IRQ_EDGE_RISE, true, readEncoder);
+  //gpio_set_irq_enabled_with_callback(A_PIN2, GPIO_IRQ_EDGE_RISE, true, readEncoder);
 }
 
 
 void loop() {
-    int speed = runningBack.printSpeed();
-    data[0] = speed & 0xFF; // Lower 8 bits
-    //Serial.println(data[0]);
-    data[1] = (speed >> 8) & 0xFF; // Upper 8 bits
+    int speed1 = runningBack1.printSpeed();
     delay(10);
+    data[0] = speed1 & 0xFF; // Lower 8 bits
+    //Serial.println(data[0]);
+    data[1] = (speed1 >> 8) & 0xFF; // Upper 8 bits
+    
+    //int speed2 = runningBack2.printSpeed();
+    //delay(10);
+    //data[2] = speed2 & 0xFF;
+    //data[3] = (speed2 >> 8) & 0xFF;
     
     //Serial.println(data[1]);
-    Serial.println(speed);
+    Serial.println(speed1);
     
     //Serial.println(test);
     uart_write_blocking(uart0, data, BYTES);
@@ -48,5 +59,5 @@ void loop() {
 }
 
 void readEncoder(uint gpio, uint32_t events) {
-  runningBack.countEncoder();
+  runningBack1.countEncoder();
 }
