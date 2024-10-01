0ab7c1c89ca25e75904460004050bd2c07f4bd81
k-colish <k-colish@onu.edu>
2024-09-30 21:01:27 -0400

    Attempts were made...

--- encoder.py
+++ encoder.py
@@ -13,36 +13,41 @@ def quadrature_encoder():
 # Need to force the program to start at address 0, using nop() until there are 32 instructions :(
 # * 
 #     ; 00 state
-    jmp('update')    # read 0000
-    jmp('decrement') # read 0001
-    jmp('increment') [20] # read 0010
-    jmp('update')    # read 0011
+    jmp("update")    # read 0000
+    jmp("increment") # read 0001
+    jmp("decrement") # read 0010
+    jmp("update")    # read 0011
 
 # ; 01 state
-    jmp('increment') # read 0100
-    jmp('update')    # read 0101
-    jmp('update')    # read 0110
-    jmp('decrement') # read 0111
+    jmp("decrement") # read 0100
+    jmp("update")    # read 0101
+    jmp("update")    # read 0110
+    jmp("increment") # read 0111
 
 # ; 10 state
-    jmp('decrement') [20] # read 1000 ( Seems to work for decrementing 9/23 )
-    jmp('update')    # read 1001
-    jmp('update')    # read 1010
-    jmp('increment') # read 1011
+    jmp("increment") # read 1000 ( Seems to work for decrementing 9/23 )
+    jmp("update")    # read 1001
+    jmp("update")    # read 1010
+    jmp("decrement") # read 1011
 
 # ; to reduce code size, the last 2 states are implemented in place and become the
 # ; target for the other jumps
 
 # ; 11 state
-    jmp('update')    # read 1100
-    jmp('increment') # read 1101
+    jmp("update")    # read 1100
+    jmp("decrement") # read 1101
+    jmp("increment") # read 1110    
+    jmp("update")    # read 1111
 
 
     label("decrement")
     jmp(y_dec, "update")
+
     
 # this is where the main loop starts
     wrap_target()
+    
+    #set(y,0)
 
     label("update")
     mov(isr, y)
@@ -58,33 +63,30 @@ def quadrature_encoder():
     mov(osr, isr)
 
 # jump to the correct state machine action
-    mov(pc, isr)
+    mov(pc, osr)    # Was isr, experimenting with osr
 
 # There is no increment operator, so we have to 
 # negate, decrement, and then negate again.
     label("increment")
     mov(y, invert(y))
-    jmp(y_dec, 'increment_cont')
+    jmp(y_dec, "increment_cont")
 
     label("increment_cont")
     mov(y, invert(y))
-    jmp('update')
-
+    jmp("update")
     wrap()
     nop()
     nop()
     nop()
     nop()
-    nop()
-    nop()
-    nop()
 
+
+sm1 = StateMachine(0, quadrature_encoder, freq=2000, set_base=Pin(1))
+sm1.active(1)
 while True:
-    sm1 = StateMachine(0, quadrature_encoder, freq=2000, set_base=Pin(1))
-    sm1.active(1)
-    print("test")
-    y_value = sm1.get()
-    print(f"Y Value: {y_value}")
-    time.sleep_ms(100)
+    # y_value = sm1.get()
+    # print((y_value))
+    print("Y Value:  %5d" % (sm1.get()))
+    time.sleep_ms(50)
 
     #sm1.irq(None, 1)
