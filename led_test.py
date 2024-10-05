d2d65d672c721548e601fca80711e499366b81ac
okquentin <qosterhage@outlook.com>
2024-10-04 20:38:13 -0400

    Update led_test.py

--- led_test.py
+++ led_test.py
@@ -3,34 +3,26 @@ from rp2 import PIO, StateMachine, asm_pio
 import rp2
 from machine import Pin
 
-@rp2.asm_pio(set_init=(PIO.IN_HIGH, PIO.IN_HIGH), out_init=(PIO.OUT_HIGH, PIO.OUT_HIGH))
+@rp2.asm_pio(out_init=(PIO.OUT_LOW), sideset_init=(PIO.OUT_LOW))
 def led_blink():
+    # nop() # read 0000, keep led off
+    # jump("turnon_led") # read 0001, turn led on
+
     wrap_target() # start here
+    label("turnon_led")
+    nop() .side(0x0)
 
-    out(isr, 2) # move 2 bits from OSR to ISR
-    in_(pins, 2) # then 2 bits from pins to ISR
+    # out(isr, 2) # move 2 bits from OSR to ISR
+    # in_(pins, 2) # then 2 bits from pins to ISR
 
-    mov(osr, isr) # Save the state in the OSR, so that we can use ISR for other purposes
+  
+    # mov(osr, isr) # Save the state in the OSR, so that we can use ISR for other purposes
+    # mov(pc, osr) # jump to the correct state machine action by moving PC
+
+    # # mov(osr, isr) # Save the state in the OSR, so that we can use ISR for other purposes
     wrap() # wrap
-    nop()
-    nop()
-    nop()
-    nop()
-    nop()
-    nop()
-    nop()
-    nop()
-    nop()
-    nop()
-    nop()
-    nop()
-    nop()
-    nop()
-    nop()
-    nop()
-    nop()
 
-sm1 = StateMachine(0, led_blink, freq=2000, set_base=Pin(1))
+sm1 = StateMachine(0, led_blink, freq=2000, sideset_base=Pin(25))
 sm1.active(1)
 while True:
-    sm1.put(1 << 3)
+    print("test")
