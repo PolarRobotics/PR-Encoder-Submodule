dd3f30c4cf8061358aee1e102f97c2a7b449a7db
okquentin <qosterhage@outlook.com>
2024-10-07 20:12:41 -0400

    Testing state machine with built in LED

--- led_test.py
+++ led_test.py
@@ -3,26 +3,66 @@ from rp2 import PIO, StateMachine, asm_pio
 import rp2
 from machine import Pin
 
-@rp2.asm_pio(out_init=(PIO.OUT_LOW), sideset_init=(PIO.OUT_LOW))
+@rp2.asm_pio(set_init=(PIO.IN_HIGH, PIO.IN_HIGH), out_init=(PIO.OUT_HIGH, PIO.OUT_HIGH), sideset_init=(PIO.OUT_LOW))
 def led_blink():
-    # nop() # read 0000, keep led off
-    # jump("turnon_led") # read 0001, turn led on
+    jmp("turnon_led") # read 0000, keep led off 
+    jmp("off") # read 0001, keep led off 
+    jmp("off") #read 0010
+    #jmp("turnon_led") #read 0011
+    jmp("off") #read 0100
+    jmp("off") #read 0101
+    jmp("off") #read 0110
 
-    wrap_target() # start here
-    label("turnon_led")
+    jmp("off") #read 0111
+    #jmp("turnon_led") #read 0111
+
+    jmp("off") #read 1000
+    jmp("off") #read 1001
+    jmp("off") #read 1010
+
+    jmp("off") #read 1011
+    #jmp("turnon_led")#read 1011
+
+    jmp("off") #read 1100
+    jmp("off") #read 1101
+    jmp("off") #read 1110
+    jmp("off") #read 1111
+    
+    label("turnon_led") 
+    nop() .side(0x1)
+
+    wrap_target() # start here 
+
+    label("update")
+    out(isr, 2) # move 2 bits from OSR to ISR
+    in_(pins, 2) # then 2 bits from pins to ISR
+    mov(osr, isr) # Save the state in the OSR, so that we can use ISR for other purposes
+    mov(pc, osr) # jmp to the correct state machine action by moving PC
+
+    label("off")
     nop() .side(0x0)
+    out(isr, 2) # move 2 bits from OSR to ISR
+    in_(pins, 2) # then 2 bits from pins to ISR
+    mov(osr, isr) # Save the state in the OSR, so that we can use ISR for other purposes
+    mov(pc, osr) # jmp to the correct state machine action by moving PC
 
-    # out(isr, 2) # move 2 bits from OSR to ISR
-    # in_(pins, 2) # then 2 bits from pins to ISR
+    # to get to 32 instructions
+    nop()
+    nop()
+    nop()    
+    nop()
+    nop()
+    nop()
+    nop()
+    #nop() 
+    #nop()
+    #nop()
+    #nop()
+    #nop()
 
-  
-    # mov(osr, isr) # Save the state in the OSR, so that we can use ISR for other purposes
-    # mov(pc, osr) # jump to the correct state machine action by moving PC
-
-    # # mov(osr, isr) # Save the state in the OSR, so that we can use ISR for other purposes
     wrap() # wrap
 
-sm1 = StateMachine(0, led_blink, freq=2000, sideset_base=Pin(25))
+sm1 = StateMachine(0, led_blink, freq=2000, set_base=Pin(0), out_shiftdir=PIO.SHIFT_RIGHT, sideset_base=Pin(25))
 sm1.active(1)
 while True:
     print("test")
